9878f817ae53d5debfdf17b5b7faa0011f5f822f
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-01-10 22:07:25 +0200

    deleted random folder

--- random/backend/middleware/error-handler.js
+++
@@ -1,9 +0,0 @@
-const { CustomAPIError } = require('../custom-error.js')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(500).json({ msg: 'Something went wrong, please try again' })
-}
-
-module.exports = errorHandlerMiddleware
