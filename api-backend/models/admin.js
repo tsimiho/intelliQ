9ba6e5315d14a20a88cb25ee62453f395e76adaa
Nikos Goutzoulias <el19647@mail.ntua.gr>
2023-01-13 23:49:58 +0200

    updated admin schema

--- api-backend/models/admin.js
+++ api-backend/models/admin.js
@@ -1,22 +1,30 @@
-const mongoose = require('mongoose')
-const QuestionnaireSchema = require('./questionnaire')
-
+const mongoose = require("mongoose");
+const QuestionnaireSchema = require("./questionnaire");
 
 const AdminSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	Organization: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	History: {
-		type: [QuestionnaireSchema],
-		required: true
-	}
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  History: {
+    type: [QuestionnaireSchema],
+    required: true,
+  },
+  Password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
 });
 
-module.exports = mongoose.model('AdminSchema', AdminSchema);
+AdminSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model("AdminSchema", AdminSchema);
