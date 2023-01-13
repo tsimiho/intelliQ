708e2192fa2e54a95d1882d48d2286848f8cfae9
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-01-14 00:19:23 +0200

    finished login

--- api-backend/controllers/login.js
+++ api-backend/controllers/login.js
@@ -1,58 +1,27 @@
 const jwt = require("jsonwebtoken");
-const { BadRequestError } = require("../errors");
 const AdminSchema = require("../models/admin");
 const bcrypt = require("bcrypt");
 
 const login = async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  if (!username || !password) {
-    throw new BadRequestError("Please provide email and password");
-  }
+    if (!username || !password) {
+        res.send(400).json({ msg: "Please provide email and password" });
+    }
 
-  const ad = AdminSchema.findOne({ usename: username });
+    const ad = AdminSchema.findOne({ username: username });
 
-  if (await bcrypt.compare(req.body.password, user.password)) {
-    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-  }
+    if (!ad || (ad && (await bcrypt.compare(password, user.password)))) {
+        res.send(401).json({ msg: "Not authorized" });
+    } else {
+        const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+            expiresIn: "30d",
+        });
 
-  res.status(200).json({ msg: "user created", token });
+        res.status(200).json({ msg: "Login Succesful", token });
+    }
 };
 
-// const dashboard = async (req, res) => {
-//     const luckyNumber = Math.floor(Math.random() * 100);
-
-//     res.status(200).json({
-//         msg: `Hello, ${req.user.username}`,
-//         secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-//     });
-// };
-
-try {
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const user = { name: req.body.name, password: hashedPassword };
-  users.push(user);
-  res.status(201).send();
-} catch {
-  res.status(500).send();
-}
-const user = users.find((user) => user.name === req.body.name);
-if (user == null) {
-  return res.status(400).send("Cannot find user");
-}
-try {
-  if (await bcrypt.compare(req.body.password, user.password)) {
-    res.send("Success");
-  } else {
-    res.send("Not Allowed");
-  }
-} catch {
-  res.status(500).send();
-}
-
 module.exports = {
-  login,
-  // dashboard,
+    login,
 };
