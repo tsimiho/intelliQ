3b2759da9e787283af4978cf1f0fca6b56151aaa
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-02-10 17:47:28 +0200

    connected QuestionPage to backend

--- frontend/src/components/Question.js
+++ frontend/src/components/Question.js
@@ -1,73 +1,75 @@
-import React, { useState } from 'react';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import RadioGroup from '@mui/material/RadioGroup';
-import Radio from '@mui/material/Radio';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { TextField } from '@mui/material';
+import React, { useState } from "react";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import RadioGroup from "@mui/material/RadioGroup";
+import Radio from "@mui/material/Radio";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { TextField } from "@mui/material";
 
 function Question(props) {
-    const [answer, setAnswer] = useState('');
+    const [answer, setAnswer] = useState("");
     const handleChange = (event) => {
         setAnswer(event.target.value);
     };
     function MultiOptions(props) {
         return (
-                <RadioGroup
-                    aria-labelledby="demo-radio-buttons-group-label"
-                    defaultValue=''
-                    name="radio-buttons-group"
-                    onChange={handleChange}
-                >
-                {props.options.map((option) =>
-                <FormControlLabel value={option.optID} control={<Radio />} label={option.opttxt} />)}
-                </RadioGroup> 
-        );     
-    };
+            <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue=""
+                name="radio-buttons-group"
+                onChange={handleChange}
+            >
+                {props.options.map((option) => (
+                    <FormControlLabel
+                        key={option._id}
+                        value={option._id}
+                        control={<Radio />}
+                        label={option.opttext}
+                    />
+                ))}
+            </RadioGroup>
+        );
+    }
     function TextOption() {
         return (
             <TextField
-            id="outlined-multiline-flexible"
-            label=''
-            //multiline
-            maxRows={4}
-            onChange={handleChange}
-            sx={{ mt: 2 , width: 400 }}
+                id="outlined-multiline-flexible"
+                label=""
+                //multiline
+                maxRows={4}
+                onChange={handleChange}
+                sx={{ mt: 2, width: 400 }}
             />
         );
-    };
+    }
     function MyButton() {
-        return (
-            (props.required === "TRUE" && answer === '') ?
-            <Button 
-                variant="contained" 
+        return props.required === "TRUE" && answer === "" ? (
+            <Button variant="contained" sx={{ mt: 3 }} disabled>
+                Next
+            </Button>
+        ) : (
+            <Button
+                href={`/doanswer/QQ000/${props._id}/ABCD/${answer}`}
+                variant="contained"
                 sx={{ mt: 3 }}
-                disabled
-                >
-                    Next
-            </Button>:
-            <Button 
-                href={`/doanswer/QQ000/${props.qID}/ABCD/${answer}`}
-                variant="contained" 
-                sx={{ mt: 3 }}
-                >
-                    Next
-            </Button>           
+            >
+                Next
+            </Button>
         );
     }
 
     return (
-       <>
-            <Typography variant="h5" style={{ marginTop: '30px' }} gutterBottom>
-                { props.qtext }
+        <>
+            <Typography variant="h5" style={{ marginTop: "30px" }} gutterBottom>
+                {props.qtext}
             </Typography>
             <FormControl>
-                {props.options.length === 1 ? TextOption() : MultiOptions(props)}
+                {props.options.length === 1
+                    ? TextOption()
+                    : MultiOptions(props)}
             </FormControl>
-            <div>
-                {MyButton()}
-            </div>       
+            <div>{MyButton()}</div>
         </>
     );
 }
--- frontend/src/components/pages/QuestionPage.js
+++ frontend/src/components/pages/QuestionPage.js
@@ -1,23 +1,46 @@
-import React from "react";
-import QExample from "../QExample";
+import React, { useState } from "react";
+// import QExample from "../QExample";
 import Question from "../Question";
-import Typography from "@mui/material/Typography";
+// import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import request from "request";
+import axios from "axios";
 
 function QuestionPage(props) {
-    const Q = QExample();
+    const [question, setQuestion] = useState({
+        qtext: "",
+        require: true,
+        qtype: "",
+        options: [],
+        _id: "",
+    });
+    // const Q = QExample();
     const { params } = props.match;
-    //   const question = Q.questions.find((q) => q.qID === params.questionID);
-    const question = request.get(
+    // const url = `/intelliq_api/question/${params.questionnaireID}/${params.questionID}`;
+
+    // const question = Q.questions.find((q) => q.qID === params.questionID);
+    console.log("hello");
+
+    console.log(
         `http://localhost:9103/intelliq_api/question/${params.questionnaireID}/${params.questionID}`
     );
 
+    axios
+        .get(
+            `http://localhost:9103/intelliq_api/question/${params.questionnaireID}/${params.questionID}`,
+            { crossdomain: true }
+        )
+        .then((response) => {
+            setQuestion(response.data.question);
+            // console.log(response.data);
+        });
+
+    console.log(question);
+
     return (
         <Container maxWidth="md" style={{ marginTop: "80px" }}>
-            <Typography variant="h4" gutterBottom>
+            {/* <Typography variant="h4" gutterBottom>
                 {Q.questionnaireTitle}
-            </Typography>
+            </Typography> */}
             <Question
                 qID={question.qID}
                 qtext={question.qtext}
