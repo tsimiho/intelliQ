14adf74760e791f615fe19aad661b4ba245af6f4
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-02-10 13:35:02 +0200

    testing QuestionPage

--- frontend/src/components/pages/QuestionPage.js
+++ frontend/src/components/pages/QuestionPage.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import QExample from '../QExample';
-import Question from '../Question';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-
+import React from "react";
+import QExample from "../QExample";
+import Question from "../Question";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import request from "request";
 
 function QuestionPage(props) {
-  const Q = QExample();
-  const { params } = props.match;
-  const question = Q.questions.find((q) => q.qID === params.questionID);;
+    const Q = QExample();
+    const { params } = props.match;
+    //   const question = Q.questions.find((q) => q.qID === params.questionID);
+    const question = request.get(
+        `http://localhost:9103/intelliq_api/question/${params.questionnaireID}/${params.questionID}`
+    );
 
-  return (
-      <Container maxWidth="md" style={{ marginTop: '80px' }}>
-        <Typography variant="h4" gutterBottom>
-          { Q.questionnaireTitle }
-        </Typography>
-          <Question 
-            qID={question.qID}
-            qtext={question.qtext} 
-            options={ question.options } 
-            required={ question.required } 
-            type={ question.type } 
-          />
-      </Container>     
-  );
+    return (
+        <Container maxWidth="md" style={{ marginTop: "80px" }}>
+            <Typography variant="h4" gutterBottom>
+                {Q.questionnaireTitle}
+            </Typography>
+            <Question
+                qID={question.qID}
+                qtext={question.qtext}
+                options={question.options}
+                required={question.required}
+                type={question.type}
+            />
+        </Container>
+    );
 }
 
-export default QuestionPage
+export default QuestionPage;
