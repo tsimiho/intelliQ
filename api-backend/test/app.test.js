33b2fc73d9bc4fb861c3120b78a5b3d773cfefef
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-02-12 18:29:52 +0200

    implemented api testing

--- api-backend/test/app.test.js
+++ api-backend/test/app.test.js
@@ -1,62 +1,133 @@
-const supertest = require("supertest");
-const app = require("../app");
-//const { MongoMemoryServer } = require("mongodb-memory-server");
-const mongoose = require("mongoose");
+const chai = require("chai");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fs = require("fs");
+const request = require("request");
 
-describe("given the product does exist", () => {
-  it("should return a 200 status and the product", async (done) => {
-    // @ts-ignore
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", baseURL + theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
 
-    const questionnaireID = "QQ000";
-    const { body, statusCode } = await supertest(app).get(
-      `/intelliq_api/healthcheck`
-    );
+function httpPost(theUrl, sent) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", baseURL + theUrl, false); // false for synchronous request
+    xmlHttp.send(sent);
+    return xmlHttp.responseText;
+}
 
-    expect(statusCode).toBe(200);
+function httpPostStatus(theUrl, sent) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", baseURL + theUrl, false); // false for synchronous request
+    xmlHttp.send(sent);
+    return xmlHttp.status;
+}
 
-    expect(body.questionnaireID).toBe(questionnaireID);
-    done();
-  });
+function httpPostUpload(theUrl, source) {
+    var req = request.post(baseURL + theUrl, function (err, resp, body) {
+        if (err) {
+            console.log("Error!");
+        } else {
+            console.log("Success!");
+        }
+    });
+
+    const data = fs.readFileSync(source);
+    var form = req.form();
+    form.append("uploaded_file", data, {
+        filename: "uploaded_file.txt",
+        contentType: "multipart/form_data",
+    });
+    return "Success!";
+}
+
+const baseURL = "http://localhost:9103/intelliq_api";
+
+describe("GET questionnaire", () => {
+    it("should print the correct output", async () => {
+        var response = httpGet("/questionnaire/QQ000");
+
+        chai.expect(JSON.parse(response)).to.have.property("questionnaireID");
+        chai.expect(JSON.parse(response)).to.have.property(
+            "questionnaireTitle"
+        );
+        chai.expect(JSON.parse(response)).to.have.property("questions");
+        chai.expect(JSON.parse(response).questions).to.be.an("array").that.is
+            .not.empty;
+    });
 });
 
-// const request = require("supertest");
-// const app = require("../app");
+describe("healthcheck", () => {
+    it("should print the correct output", async () => {
+        var response = httpGet("/admin/healthcheck");
 
-// describe("get Endpoints", () => {
-//   // it("should create a new post", async () => {
-//   //   const res = await request(app).post("/api/posts").send({
-//   //     userId: 1,
-//   //     title: "test is cool",
-//   //     content: "Lorem ipsum",
-//   //   });
-//   //   expect(res.statusCode).toEqual(201);
-//   //   expect(res.body).toHaveProperty("post");
-//   // });
+        chai.expect(JSON.parse(response)).to.have.property("status");
+        chai.expect(JSON.parse(response).status).to.equal("OK");
+    });
+});
 
-//   it("should get a single post", async (done) => {
-//     const questionnaireID = "QQ000";
-//     const res = await request(app).get(
-//       `/intelliq_api/questionnaire/${questionnaireID}`
-//     );
-//     expect(res.statusCode).toEqual(200);
-//     expect(res.body).toHaveProperty("questionnaireTitle");
-//     done();
-//   });
-//   // it("should fetch all posts", async () => {
-//   //   const res = await request(app).get("/api/posts");
-//   //   expect(res.statusCode).toEqual(200);
-//   //   expect(res.body).toHaveProperty("posts");
-//   //   expect(res.body.posts).toHaveLength(1);
-//   // });
+describe("GET question", () => {
+    it("should print the correct output", async () => {
+        var response = httpGet("/question/QQ000/Q01");
 
-//   // it("should update a post", async () => {
-//   //   const res = await request(app).put("/api/posts/1").send({
-//   //     userId: 1,
-//   //     title: "updated title",
-//   //     content: "Lorem ipsum",
-//   //   });
+        chai.expect(JSON.parse(response)).to.have.property("qID");
+        chai.expect(JSON.parse(response)).to.have.property("qtext");
+        chai.expect(JSON.parse(response)).to.have.property("options");
+        chai.expect(JSON.parse(response).options).to.have.be.an("array").that.is
+            .not.empty;
+    });
+});
 
-//   //   expect(res.statusCode).toEqual(200);
-//   //  / expect(res.body).toHaveProperty("post");
-//   //   expect(res.body.post).toHaveProperty("title", "updated title");
+describe("GET sessionanswers", () => {
+    it("should print the correct output", async () => {
+        var response = httpGet("/getsessionanswers/QQ000/SS000");
+
+        chai.expect(JSON.parse(response).answers).to.be.an("array").that.is.not
+            .empty;
+    });
+});
+
+describe("GET questionanswers", () => {
+    it("should print the correct output", async () => {
+        var response = httpGet("/getquestionanswers/QQ000/Q01");
+
+        chai.expect(JSON.parse(response).answers).to.be.an("array").that.is.not
+            .empty;
+    });
+});
+
+describe("doanswer", () => {
+    it("should print the correct output", async () => {
+        var response = httpPostStatus("/doanswer/QQ000/Q06/SS000/Q06A1");
+
+        chai.expect(response).to.equal(200);
+    });
+});
+
+describe("questionnaire upload", () => {
+    it("should print the correct output", async () => {
+        var response = httpPostUpload(
+            "/admin/questionnaire_upd",
+            "./example.json"
+        );
+
+        chai.expect(response).to.equal("Success!");
+    });
+});
+
+// describe("reset q", () => {
+//     it("should print the correct output", async () => {
+//         var response = httpPost("/admin/resetq/QQ000");
+//         chai.expect(JSON.parse(response)).to.have.property("status");
+//         chai.expect(JSON.parse(response).status).to.equal("OK");
+//     });
+// });
+
+// describe("reset all", () => {
+//     it("should print the correct output", async () => {
+//         var response = httpPost("/admin/resetall");
+//         chai.expect(JSON.parse(response)).to.have.property("status");
+//         chai.expect(JSON.parse(response).status).to.equal("OK");
+//     });
 // });
