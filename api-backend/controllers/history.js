822709770970d7ee9751ea442dacc793bb1f9d63
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-02-14 14:28:27 +0200

    fixed 500 again

--- api-backend/controllers/history.js
+++ api-backend/controllers/history.js
@@ -44,9 +44,9 @@ const history = async (req, res) => {
             }
 
             if (req.query && req.query.format === "csv") {
-                res.status(200).send(json2csv(qnas));
+                res.status(200).send(json2csv(result));
             } else {
-                res.status(200).json(qnas);
+                res.status(200).json(result);
             }
         }
     } catch (error) {
