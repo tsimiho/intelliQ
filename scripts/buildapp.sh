f1c816c40bfa05ace2adeb9a67e50b25c3dede5f
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-02-17 03:01:39 +0200

    added sudo in scripts

--- scripts/buildapp.sh
+++ scripts/buildapp.sh
@@ -2,5 +2,5 @@
 
 gnome-terminal -- sh -c "cd ~/Desktop && rm -rf SoftEng22-44 && git clone https://github.com/ntua/SoftEng22-44 && 
 (gnome-terminal -- sh -c 'cd ~/Desktop/SoftEng22-44/api-backend && npm install && npm start') && 
-(gnome-terminal -- sh -c 'cd ~/Desktop/SoftEng22-44/cli && npm install && sudo rm -rf /usr/local/lib/node_modules/cli && npm install -g .') && 
+(gnome-terminal -- sh -c 'cd ~/Desktop/SoftEng22-44/cli && npm install && sudo rm -rf /usr/local/lib/node_modules/cli && sudo npm install -g .') && 
 (gnome-terminal -- sh -c 'cd ~/Desktop/SoftEng22-44/frontend && npm install && npm start')"
