735221f601054f8ea9dad76c31931bfd9e72745e
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-02-17 15:00:16 +0200

    changed a script

--- scripts/buildapp.sh
+++ scripts/buildapp.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-gnome-terminal -- sh -c "cd ~/Desktop && rm -rf SoftEng22-44 && git clone https://github.com/ntua/SoftEng22-44 && 
-(gnome-terminal -- sh -c 'cd ~/Desktop/SoftEng22-44/api-backend && npm install && npm start') && 
-(gnome-terminal -- sh -c 'cd ~/Desktop/SoftEng22-44/cli && npm install && sudo rm -rf /usr/local/lib/node_modules/cli && sudo npm install -g .') && 
-(gnome-terminal -- sh -c 'cd ~/Desktop/SoftEng22-44/frontend && npm install && npm start')"
+gnome-terminal -- sh -c "(gnome-terminal -- sh -c 'cd ../api-backend && npm install && npm start') && 
+(gnome-terminal -- sh -c 'cd ../cli && npm install && sudo rm -rf /usr/local/lib/node_modules/cli && sudo npm install -g .') && 
+(gnome-terminal -- sh -c 'cd ../frontend && npm install && npm start')"
